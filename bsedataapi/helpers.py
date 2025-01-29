3be81c0f8f2e3332ffb9d37e795d3a9d79003418
paul antony <paulantonytoc@gmail.com>
2025-01-29 15:10:14 +0530

    fixed spelling mistae in requirments

--- bsedataapi/helpers.py
+++ bsedataapi/helpers.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver-manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
